f3c92c7f2f8f8867b792c2825e683f962e4fe512
ZiyadCh <chetoziyad@gmail.com>
2026-09-07 17:14:49 +0100

    created abstract person and its child client

---
+++ Person.java
@@ -0,0 +1,15 @@
+abstract class Person {
+  protected String nom;
+  protected String prenom;
+  protected String email;
+  protected String password;
+
+  protected void Login() {
+
+  }
+
+  protected void Logout() {
+
+  }
+
+}
